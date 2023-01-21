9cb475b6ad9b794282f5e853eda0a36d30035e84
bandit-dynamic <bandit.dynamic@protonmail.com>
2023-01-21 16:45:26 -0700

    html up to the body made

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// console.log("hello world")
+
+// create buttons
+const sleepBtn = document.querySelector("#action-sleep");
+const feedBtn = document.querySelector("#action-feed");
+const playBtn = document.querySelector("#action-play");
+const startBtn = document.querySelector("#action-menu-start-game");
+const settingsBtn = document.querySelector("#action-menu-settings");
+const settingsBackBtn = document.querySelector("#action-settings-back");
